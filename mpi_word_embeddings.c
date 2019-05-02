cfe566805d5365857166c02b765a6fef353cf0a7
Lucia E <lesan2807@gmail.com>
2019-05-01 23:14:28 -0600

    slave processes are receiving the words that are distributed evenly for each process

--- mpi_word_embeddings.c
+++ mpi_word_embeddings.c
@@ -5,6 +5,8 @@
 
 #define MIN(a,b) (((a)<=(b))?(a):(b))
 
+#define MAX_WORD_LENGTH 50
+
 
 int main(int argc, char *argv[])
 {
@@ -18,63 +20,79 @@ int main(int argc, char *argv[])
 	// get process_count 
 	MPI_Comm_size(MPI_COMM_WORLD, &process_count);
 
-	int word_matrix_row_size = 10;  
-	int word_matrix_col_size = 300; 
-
-	// Get quotient and remainder to use a formula to calculate start and final 
-	int c = word_matrix_row_size / process_count;
-	int r = word_matrix_row_size % process_count;
-	int start = my_rank * c + MIN((long long)my_rank, (long long)r);
-	int final = (my_rank+1) * c + MIN((long long)(my_rank+1), (long long)r);
-	int range = final-start; 
-
-
-	// Open file, change this for your working directory. 
-	FILE* word_embedding_file = fopen("/home/lesan2807/Documents/MPIImplementation/Untitled Folder/word-embedding/word_embeddings_small.txt", "r");
-	if( !word_embedding_file)
+	if(process_count < 3)
 	{
-		printf("%s\n", "Could not open file. Ending program");
+		if(my_rank == 0)
+			printf("%s\n", "For this program we want more than 1 slave process");
 		MPI_Finalize(); 
 		return 1; 
 	}
 
-	printf("[%d-%d[: %d\n", start, final, range);
+	int word_matrix_row_size = 10;  
+	int word_matrix_col_size = 5; 
+
+	// Get quotient and remainder to use a formula to calculate start and final 
+	int c = word_matrix_row_size / (process_count-1);
+	int r = word_matrix_row_size % (process_count-1);
+	int start = my_rank * c + MIN((long long)my_rank, (long long)r);
+	int final = (my_rank+1) * c + MIN((long long)(my_rank+1), (long long)r);
+	int range = final-start; 
+
+	//printf("[%d-%d[: %d\n", start, final, range);
 	// Create an array for storing the words 
-	char* words = (char*)malloc((final-start ) * sizeof(char)); 
+	char** words = (char**) calloc(range, sizeof(char*)); 
+	for( int row_index = 0; row_index < range; ++row_index)
+		words[row_index] = (char*) calloc(MAX_WORD_LENGTH, sizeof(char));	
 
 	// Create word embeddings matrix 
-	// float** word_matrix = (float**)malloc( range * sizeof(float*));
-	// for ( int row_index = 0; row_index <	 word_matrix_row_size; ++row_index )
-	// 	word_matrix[row_index] = (float*)malloc(word_matrix_col_size * sizeof(float));
-	float** word_matrix = calloc(range, sizeof(float*)); 
+	float** word_matrix = (float**) calloc(range, sizeof(float*)); 
 	for( int row_index = 0; row_index < range; ++row_index)
-		word_matrix[row_index] = calloc(word_matrix_col_size, sizeof(float));	
+		word_matrix[row_index] = (float*) calloc(word_matrix_col_size, sizeof(float));	
 
 
-	if(my_rank == 0)
+	// Open file, change this for your working directory. 
+	FILE* word_embedding_file = fopen("/home/lesan2807/Documents/MPIImplementation/word_embedding/test.txt", "r");
+	if( !word_embedding_file)
 	{
-		int count_words = 0; 
-		for(int row_index = 0; row_index < word_matrix_row_size; ++row_index) // TODO: THIS PRODUCES SEgFAULT FIX IT
+		printf("%s\n", "Could not open file. Ending program");
+		MPI_Finalize(); 
+		return 2; 
+	}
+
+
+	if(my_rank == 0) // Process 0 reads all the matrix sends sub matrix to others 
+	{
+		int num_process_destination = 1; 
+		for(int row_index = 0; row_index < word_matrix_row_size; ++row_index) 
 		{
+			int distribute_index = row_index % range; 
 			for(int col_index = 0; col_index < word_matrix_col_size; ++col_index)
 			{
 				if(col_index == 0)
 				{
-					fscanf(word_embedding_file, "%s", words+count_words); 
-					//printf("%s\t", words+count_words);
-					++count_words; 
+					fscanf(word_embedding_file, "%s", words[distribute_index]); 
+					printf("%s\t", words[distribute_index]);
 				}
-				fscanf(word_embedding_file, "%f", &word_matrix[row_index][col_index]); 
-				//printf("%0.16f\t", word_matrix[row_index][col_index]);
+				fscanf(word_embedding_file, "%f", &word_matrix[distribute_index][col_index]); 
+				//printf("%0.16f\t", word_matrix[distribute_index][col_index]);
 			}
-			//printf("\n");
+			if(distribute_index == range-1)
+			{
+				MPI_Send(&words[0][0], (range + 1) * MAX_WORD_LENGTH, MPI_UNSIGNED_CHAR, num_process_destination, 0, MPI_COMM_WORLD);
+				//MPI_Send(word_matrix, word_matrix_row_size * word_matrix_col_size, MPI_FLOAT, num_process_destination, 0, MPI_COMM_WORLD); 
+				++num_process_destination;  
+			}
 		}
 	}
+	else
+	{
+		MPI_Recv(&words[0][0], (range + 1) * MAX_WORD_LENGTH, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
+		printf("%s%d\n", "I am process #", my_rank);
+		for(int index = 0; index < range; ++index)
+			printf("%s\n", words[index]);
+	}
 
-
-	// free memory 
-	free(words); 
 
 	for ( int row_index = 0; row_index < range; ++row_index )
 		free(word_matrix[row_index]);

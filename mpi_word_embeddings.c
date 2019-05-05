f093d0a149ed143f4477b6c25392e40ecf6cc0fe
Lucia E <lesan2807@gmail.com>
2019-05-05 09:54:23 -0600

    Divides matrix into slaves process correctly

--- mpi_word_embeddings.c
+++ mpi_word_embeddings.c
@@ -5,10 +5,10 @@
 
 #define MIN(a,b) (((a)<=(b))?(a):(b))
 
-#define MAX_WORD_LENGTH 1024
+#define MAX_WORD_LENGTH 10
 
 // change this for your directory
-#define FILENAME "/home/lesan2807/Documents/MPIImplementation/word_embedding/word_embeddings_small.txt"
+#define FILENAME "word_embeddings_small.txt"
 
 int main(int argc, char *argv[])
 {
@@ -40,11 +40,13 @@ int main(int argc, char *argv[])
 	int final = (my_rank) * c + MIN((long long)(my_rank), (long long)r);
 	int range = abs(final-start); // rank 0 has to have a size 
 
+	//printf("Range: [%d, %d[ from process %d\n", start, final, my_rank);
+
 	// Create an array for storing the words 
-	char* words = (char*) calloc(range * MAX_WORD_LENGTH, sizeof(char)); 
+	char* words = (char*) calloc(word_matrix_row_size * MAX_WORD_LENGTH, sizeof(char)); 
 
 	// Create word embeddings matrix with contiguos memory 
-	float* word_matrix = (float*) calloc(word_matrix_row_size * MAX_WORD_LENGTH, sizeof(float)); 
+	double* word_matrix = (double*) calloc(word_matrix_row_size * word_matrix_col_size, sizeof(double)); 
 
 
 	// Open file,
@@ -60,41 +62,47 @@ int main(int argc, char *argv[])
 	if(my_rank == 0) // Process 0 reads all the matrix sends sub matrix to others 
 	{
 		int num_process_destination = 1; 
-		for(int row_index = 0; row_index < word_matrix_row_size; ++row_index) 
+		for(int row_index = 0; row_index <= word_matrix_row_size; ++row_index) 
 		{
-			int distribute_index = row_index % range; 
+			int distribute_index = row_index % (range - 1); 
 			for(int col_index = 0; col_index < word_matrix_col_size; ++col_index)
 			{
 				if(col_index == 0)
 				{
 					fscanf(word_embedding_file, "%s", &words[row_index * MAX_WORD_LENGTH + col_index]); 
 				}
-				fscanf(word_embedding_file, "%f", &word_matrix[distribute_index * word_matrix_col_size + col_index]); 
-				printf("%0.16f\t", word_matrix[distribute_index * word_matrix_col_size + col_index]);
+				fscanf(word_embedding_file, "%lf", &word_matrix[distribute_index * word_matrix_col_size + col_index]); 
+				//printf("%0.16f\t", word_matrix[distribute_index * word_matrix_col_size + col_index]);
 			}
-			// if(distribute_index == range-1)
-			// {
-			// 	MPI_Send(&words[0][0], (range ) * MAX_WORD_LENGTH, MPI_UNSIGNED_CHAR, num_process_destination, 0, MPI_COMM_WORLD);
-			// 	MPI_Send(&word_matrix[0][0], (range ) * word_matrix_col_size, MPI_FLOAT, num_process_destination, 1, MPI_COMM_WORLD); 
-			// 	++num_process_destination;  
-			// }
+			if(distribute_index == 0 )
+			{
+				if(num_process_destination < process_count)
+				{
+					//printf("Sending to %d...\n", num_process_destination);
+					MPI_Send(words, word_matrix_row_size * MAX_WORD_LENGTH , MPI_UNSIGNED_CHAR, num_process_destination, 0, MPI_COMM_WORLD);
+					MPI_Send(word_matrix,  (range - 1) * word_matrix_col_size, MPI_DOUBLE, num_process_destination, 1, MPI_COMM_WORLD); 
+					num_process_destination++;  
+				}
+			}
+			printf("%d\n", distribute_index);
 		}
 	}
-	// else
-	// {
-	// 	MPI_Recv(&words[0][0], (range ) * MAX_WORD_LENGTH, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	// 	MPI_Recv(&word_matrix[0][0], (range ) * word_matrix_col_size, MPI_FLOAT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
+	else
+	{
+		MPI_Recv(words, word_matrix_row_size * MAX_WORD_LENGTH, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Recv(word_matrix, range * word_matrix_col_size, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
 
-	// 	// printf("%s%d\n", "I am process #", my_rank);
-	// 	// for(int index = 0; index < range; ++index)
-	// 	// {
-	// 	// 	for(int jndex = 0; jndex < word_matrix_col_size; ++jndex)
-	// 	// 	{
-	// 	// 		printf("[%f]", word_matrix[index][jndex]);
-	// 	// 	}
-	// 	// 	printf("%s%d\n", "from process#", my_rank);
-	// 	// }
-	// }
+		printf("%s%d\n", "I am process #", my_rank);
+
+	// 	for(int index = 0; index < range; ++index)
+	// 	{
+	// 		for(int jndex = 0; jndex < word_matrix_col_size; ++jndex)
+	// 		{
+	// 			printf("[%lf]", word_matrix[index * range + jndex]);
+	// 		}
+	// 		printf("%s%d\n", "from process#", my_rank);
+	// 	}
+	 }
 
 
 	free(words); 
